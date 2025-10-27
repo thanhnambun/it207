dc842968bbc06c94a49fddfec9eb1ce18017f2c3
nguyenthanhnam10a2lqd@gmail.com <email@domain.com>
2025-10-27 08:03:01 +0700

    ss20

--- ss19/app/_layout.tsx
+++ ss19/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 import React from "react";
 
 const queryClient = new QueryClient();
-
+  
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
